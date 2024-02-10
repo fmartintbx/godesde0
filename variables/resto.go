05439ef07765fe6e01dc30f7f783c00ef1fed65d
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-02-10 00:57:10 -0300

    Siguiente Commit

---
+++ variables/resto.go
@@ -0,0 +1,32 @@
+package variables
+
+import ( 
+	"fmt"
+	"time"
+	"strconv"
+
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha  time.Time
+
+func RestoVariables(){ 
+
+	Nombre = "Facundo"
+	Estado = true
+	Sueldo = 1577.66
+	Fecha = time.Now()
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+
+}
+func ConviertoaTexto(numero int) (bool, string){ 
+	texto := strconv.Itoa((numero))
+	return true, texto
+
+
+}
