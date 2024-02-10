c1a371e1be919dd8a9644ae36a02d5000adfe0a9
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-02-10 01:59:25 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
-
-   "github.com/fmartintbx/godesde0/variables"
+	"github.com/fmartintbx/godesde0/ejercicios"
 )
 func main(){ 
-	estado, texto := variables.ConviertoaTexto(1588)
+	/*estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
+	fmt.Println(texto)*/
+	/*if os := runtime.GOOS; os== "Linux." || os == "OS X."{
+		fmt.Println("Esto no es Windows, es", os) 
+	}else{ 
+        fmt.Println("Esto es Windows")   
+	}
+	switch os := runtime.GOOS; os{ 
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)	
+	} */
+    numero, texto := ejercicios.ConvNum("500")
+	fmt.Println(numero)
 	fmt.Println(texto)
 }
